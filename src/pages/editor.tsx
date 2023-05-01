6946c768cec7e8d4fda347e6f488eed0d94590bc
atworks <asotoruworks@gmail.com>
2023-05-01 20:32:42 +0900

    localStorageを利用

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -45,15 +45,21 @@ const Preview = styled.div`
   width: 50vw;
 `;
 
+const StorageKey = "pages/editor:text";
+
 export const Editor: React.FC = () => {
-  const [text, setText] = useState<string>("");
+  const [text, setText] = useState<string>(
+    localStorage.getItem(StorageKey) || ""
+  );
   return (
     <>
       <Header>Markdown Editor</Header>
       <Wrapper>
         <TextArea
           onChange={(event) => {
-            setText(event.target.value);
+            const changedText = event.target.value;
+            localStorage.setItem(StorageKey, changedText);
+            setText(changedText);
           }}
           value={text}
         />
